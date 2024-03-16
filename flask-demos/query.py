e0c0630521e1887def48bfd1755a9caee632ff93
ZhuJunray <96456375+ZhuJunray@users.noreply.github.com>
2024-03-16 02:11:44 -0700

    Update OpenAI API key

--- flask-demos/query.py
+++ flask-demos/query.py
@@ -2,7 +2,7 @@ import requests
 import openai
 import ipdb
 
-openai.api_key = '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'
+openai.api_key = ''
 
 content = "请给我用python的flask生成对应网页。网页中有三个文本框，下面和中间的文本框可以编辑，上面的文本框不能编辑。用户可以点击高亮按钮，点击后选择高亮颜色。选择高亮颜色后用户可以对具体内容进行划选高亮。用户点击生成按钮后需要对http://xxx.xxx.xxx.xxx:5000/chat接口发送POST请求，POST请求内容是json, json的键是user_input，值是用户输入的html。返回结果呈现在上面的文本框中。上面的文本框和下面的文本框每当有新内容的时候，需要同步给中间的文本框，也就是要在中间的文本框中增加新内容。"
 
